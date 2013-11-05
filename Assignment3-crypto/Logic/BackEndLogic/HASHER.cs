c1dc9e73a05b91bbd3759d8e0fdcbfc0ae2bf508
lordalek <alekspett@gmail.com>
2013-11-05 23:39:43 +0100

    initial commit

---
+++ Assignment3-crypto/Logic/BackEndLogic/HASHER.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment3_crypto.Logic.Interfaces;
+using System.Numerics;
+
+namespace Assignment3_crypto.Logic.BackEndLogic
+{
+  public  class Hasher : IBlock
+    {
+        #region IBlock Members
+
+        public string AddendPadding(string rawText, int paddingLength)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AppendLength(string rawTest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetHashBuffer()
+        {
+            var buffer = new string[8];
+            buffer[0] = "0110101000001001111001100110011111110011101111001100100000000000";
+            buffer[1] = "1011101101100111101011101000010110000100110010101010100000000000";
+            buffer[2] = "0011110001101110111100110111001011111110100101001111100000000000";
+            buffer[3] = "1010010101001111111101010011101001011111000111010011100000000000";
+            buffer[4] = "0101000100001110010100100111111110101101111001101000000000000000";
+            buffer[5] = "1001101100000101011010001000110000101011001111100111000000000000";
+            buffer[6] = "0001111110000011110110011010101111111011010000011011110100000000";
+            buffer[7] = "0101101111100000110011010001100100010011011111100010000000000000";
+            return buffer;
+        }
+
+        #endregion
+    }
+}
---
+++ Assignment3-crypto/Logic/Interfaces/IBlock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_crypto.Logic.Interfaces
+{
+    public interface IBlock
+    {
+        string AddendPadding(string rawText, int paddingLength);
+        string AppendLength(string rawTest);
+    }
+}
